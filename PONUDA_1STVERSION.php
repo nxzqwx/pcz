23aa8fcfab0fa6248483d07ad9d6938daea45067
nxzqwx <39726505+nxzqwx@users.noreply.github.com>
2018-05-30 17:41:05 +0200

    PONUDA_1STVERSION.php
    changes due to broken css link

---
+++ PONUDA_1STVERSION.php
@@ -0,0 +1,157 @@
+<link rel="stylesheet" type="text/css" href="https://slavkoplus.plavipixel.com.hr/wp-content/themes/pcz/css/front/all.css" />
+<link rel="stylesheet" href="style2.css">
+<article class="single__content-article" id="6580">
+
+                    <header class="ponuda__bg" style="background-image: url('https://slavkoplus.plavipixel.com.hr/wp-content/uploads/20150616-640x480-Mladenci-edit.jpeg')">
+        <section class="content">
+          <div class="ponuda__content-row">
+            <div class="ponuda__content-top--l">
+              <h1>Za mladence</h1>
+              <h4>Prvi ples za pamćenje</h4>
+              <p></p><p>Jedna od prvih stvari kojom mladenci ulaze u zajednički život jest PLES. Kao takav zaslužuje veliku pažnju i zahtjeva pripremu, jer ostaje u trajnom sjećanju za cijeli život.</p>
+<p></p>
+            </div>
+            <div class="ponuda__content-top--r">
+              <p class="ponuda__content-forma">
+
+                </p><div class="gf_browser_chrome gform_wrapper" id="gform_wrapper_4"><a id="gf_4" class="gform_anchor"></a><form method="post" enctype="multipart/form-data" target="gform_ajax_frame_4" id="gform_4" action="/ponuda/za-mladence/#gf_4">
+                        <div class="gform_body"><ul id="gform_fields_4" class="gform_fields top_label form_sublabel_below description_below"><li id="field_4_8" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_4_8">Ime<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_text"><input name="input_8" id="input_4_8" type="text" value="" class="large" tabindex="12" placeholder="Ime *" aria-required="true" aria-invalid="false"></div></li><li id="field_4_9" class="gfield gplaceholder gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_4_9">Prezime<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_text"><input name="input_9" id="input_4_9" type="text" value="" class="large" tabindex="13" placeholder="Prezime *" aria-required="true" aria-invalid="false"></div></li><li id="field_4_2" class="gfield gplaceholder gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_4_2">Kontakt telefon<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_phone"><input name="input_2" id="input_4_2" type="text" value="" class="large" tabindex="14" placeholder="Kontakt telefon *" aria-required="true" aria-invalid="false"></div></li><li id="field_4_3" class="gfield gplaceholder gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_4_3">Email<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_email">
+                            <input name="input_3" id="input_4_3" type="text" value="" class="large" tabindex="15" placeholder="Email *" aria-required="true" aria-invalid="false">
+                        </div></li><li id="field_4_11" class="gfield gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label" for="input_4_11">Dolazim<span class="gfield_required">*</span></label><div class="ginput_container ginput_container_select"><select name="input_11" id="input_4_11" class="large gfield_select" tabindex="16" aria-required="true" aria-invalid="false"><option value="" selected="selected" class="gf_placeholder">Dolazim *</option><option value="Sam/a">Sam/a</option><option value="U paru">U paru</option></select></div></li><li id="field_4_12" class="gfield field_sublabel_below field_description_below gfield_visibility_visible"><label class="gfield_label">Newsletter prijava</label><div class="ginput_container ginput_container_checkbox"><ul class="gfield_checkbox" id="input_4_12"><li class="gchoice_4_12_1">
+								<input name="input_12.1" type="checkbox" value="Newsletter prijava" checked="checked" id="choice_4_12_1" tabindex="17">
+								<label for="choice_4_12_1" id="label_4_12_1">Newsletter prijava</label>
+							</li></ul></div></li>
+                            </ul></div>
+        <div class="gform_footer top_label"> <input type="submit" id="gform_submit_button_4" class="gform_button button" value="Pošalji" tabindex="18" onclick="if(window[&quot;gf_submitting_4&quot;]){return false;}  window[&quot;gf_submitting_4&quot;]=true;  " onkeypress="if( event.keyCode == 13 ){ if(window[&quot;gf_submitting_4&quot;]){return false;} window[&quot;gf_submitting_4&quot;]=true;  jQuery(&quot;#gform_4&quot;).trigger(&quot;submit&quot;,[true]); }"> <input type="hidden" name="gform_ajax" value="form_id=4&amp;title=&amp;description=&amp;tabindex=12">
+            <input type="hidden" class="gform_hidden" name="is_submit_4" value="1">
+            <input type="hidden" class="gform_hidden" name="gform_submit" value="4">
+
+            <input type="hidden" class="gform_hidden" name="gform_unique_id" value="">
+            <input type="hidden" class="gform_hidden" name="state_4" value="WyJbXSIsIjBiMjhmNDUyZGQzMGQyMmYxODA2ODhmMWY2ZWNjZjIyIl0=">
+            <input type="hidden" class="gform_hidden" name="gform_target_page_number_4" id="gform_target_page_number_4" value="0">
+            <input type="hidden" class="gform_hidden" name="gform_source_page_number_4" id="gform_source_page_number_4" value="1">
+            <input type="hidden" name="gform_field_values" value="">
+
+        </div>
+                        </form>
+                        </div>
+                <iframe style="display:none;width:0px;height:0px;" src="about:blank" name="gform_ajax_frame_4" id="gform_ajax_frame_4">This iframe contains the logic required to handle Ajax powered Gravity Forms.</iframe>
+                <script type="text/javascript">jQuery(document).ready(function($){gformInitSpinner( 4, 'https://slavkoplus.plavipixel.com.hr/wp-content/plugins/gravityforms/images/spinner.gif' );jQuery('#gform_ajax_frame_4').on('load',function(){var contents = jQuery(this).contents().find('*').html();var is_postback = contents.indexOf('GF_AJAX_POSTBACK') >= 0;if(!is_postback){return;}var form_content = jQuery(this).contents().find('#gform_wrapper_4');var is_confirmation = jQuery(this).contents().find('#gform_confirmation_wrapper_4').length > 0;var is_redirect = contents.indexOf('gformRedirect(){') >= 0;var is_form = form_content.length > 0 && ! is_redirect && ! is_confirmation;if(is_form){jQuery('#gform_wrapper_4').html(form_content.html());if(form_content.hasClass('gform_validation_error')){jQuery('#gform_wrapper_4').addClass('gform_validation_error');} else {jQuery('#gform_wrapper_4').removeClass('gform_validation_error');}setTimeout( function() { /* delay the scroll by 50 milliseconds to fix a bug in chrome */ jQuery(document).scrollTop(jQuery('#gform_wrapper_4').offset().top); }, 50 );if(window['gformInitDatepicker']) {gformInitDatepicker();}if(window['gformInitPriceFields']) {gformInitPriceFields();}var current_page = jQuery('#gform_source_page_number_4').val();gformInitSpinner( 4, 'https://slavkoplus.plavipixel.com.hr/wp-content/plugins/gravityforms/images/spinner.gif' );jQuery(document).trigger('gform_page_loaded', [4, current_page]);window['gf_submitting_4'] = false;}else if(!is_redirect){var confirmation_content = jQuery(this).contents().find('.GF_AJAX_POSTBACK').html();if(!confirmation_content){confirmation_content = contents;}setTimeout(function(){jQuery('#gform_wrapper_4').replaceWith(confirmation_content);jQuery(document).scrollTop(jQuery('#gf_4').offset().top);jQuery(document).trigger('gform_confirmation_loaded', [4]);window['gf_submitting_4'] = false;}, 50);}else{jQuery('#gform_4').append(contents);if(window['gformRedirect']) {gformRedirect();}}jQuery(document).trigger('gform_post_render', [4, current_page]);} );} );</script><script type="text/javascript"> if(typeof gf_global == 'undefined') var gf_global = {"gf_currency_config":{"name":"Euro","symbol_left":"","symbol_right":"&#8364;","symbol_padding":" ","thousand_separator":".","decimal_separator":",","decimals":2},"base_url":"https:\/\/slavkoplus.plavipixel.com.hr\/wp-content\/plugins\/gravityforms","number_formats":[],"spinnerUrl":"https:\/\/slavkoplus.plavipixel.com.hr\/wp-content\/plugins\/gravityforms\/images\/spinner.gif"};jQuery(document).bind('gform_post_render', function(event, formId, currentPage){if(formId == 4) {if(typeof Placeholders != 'undefined'){
+                        Placeholders.enable();
+                    }} } );jQuery(document).bind('gform_post_conditional_logic', function(event, formId, fields, isInit){} );</script><script type="text/javascript"> jQuery(document).ready(function(){jQuery(document).trigger('gform_post_render', [4, 1]) } ); </script>              <p></p>
+            </div>
+
+          </div>
+        </section>
+      </header>
+      <section class="content__single-title">
+        <div class="content">
+
+
+          <div class="content__single-title-h1">
+             <h2>
+               <span class="pp-roza">Plesovi:</span> engleski valcer, bečki valcer, foxtrot (dvokorak u kombinaciji s disco-foxom), blues (sentiš).             </h2>
+          </div>
+
+          <div class="content__single-sharing">
+            <h5 class="content__sharing">PODIJELITE SADRŽAJ</h5>
+            <div class="content__sharing-button">
+              <a target="_blank" class="share--button share--twitter" href="https://twitter.com/intent/tweet?url=https://slavkoplus.plavipixel.com.hr/ponuda/za-mladence/&amp;text=Za mladence&amp;via=" title="Tweet this">
+                <object type="image/svg+xml" data="https://slavkoplus.plavipixel.com.hr/wp-content/themes/pcz/files/img/twitter-social.svg">
+                </object>
+              </a>
+              <a target="_blank" class="share--button share--facebook" href="https://www.facebook.com/sharer/sharer.php?u=https://slavkoplus.plavipixel.com.hr/ponuda/za-mladence/" title="Share on Facebook">
+                <object type="image/svg+xml" data="https://slavkoplus.plavipixel.com.hr/wp-content/themes/pcz/files/img/facebook-social.svg">
+                </object>
+              </a>
+              <a target="_blank" class="share--button share--googleplus" href="https://plus.google.com/share?url=https://slavkoplus.plavipixel.com.hr/ponuda/za-mladence/" title="Share on Google+">
+                <object type="image/svg+xml" data="https://slavkoplus.plavipixel.com.hr/wp-content/themes/pcz/files/img/google-social.svg">
+                </object>
+                </a>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+    <section>
+      <div class="content">
+        <div class="ponuda__tablica">
+
+<table id="tablepress-15" class="tablepress tablepress-id-15">
+<caption style="caption-side:bottom;text-align:left;border:none;background:none;margin:0;padding:0;"><a href="https://slavkoplus.plavipixel.com.hr/wp-admin/admin.php?page=tablepress&amp;action=edit&amp;table_id=15">Uredi</a></caption>
+<thead>
+<tr class="row-1 odd">
+	<th class="column-1">Naziv</th><th class="column-2">Dani u tjednu</th><th class="column-3">Satnica</th><th class="column-4">Status / Termin</th><th class="column-5">&nbsp;</th>
+</tr>
+</thead>
+<tbody class="row-hover">
+<tr class="row-2 even">
+	<td colspan="4" class="column-1"><h4>BRZI TEČAJEVI ZA MLADENCE I SVE OSTALE</h4></td><td class="column-5">Jednokratno/ dvokratno</td>
+</tr>
+<tr class="row-3 odd">
+	<td class="column-1">Ubrzani dvotjedni tečaj 9 sati</td><td class="column-2">PON/UTO/PET</td><td class="column-3">20:30 - 22:00</td><td class="column-4"><strong>Planirani početak 28.5.</strong></td><td class="column-5">350 kn</td>
+</tr>
+<tr class="row-4 even">
+	<td colspan="4" class="column-1"></td><td class="column-5"></td>
+</tr>
+<tr class="row-5 odd">
+	<td class="column-1">Brzi vikend tečaj 6 sati</td><td class="column-2">PET/ SUB/NED</td><td class="column-3">20:00 - 22:00</td><td class="column-4"><strong>Planirani početak 8.6.</strong></td><td class="column-5">250 kn </td>
+</tr>
+</tbody>
+</table>
+
+<table id="tablepress-5" class="tablepress tablepress-id-5">
+<caption style="caption-side:bottom;text-align:left;border:none;background:none;margin:0;padding:0;"><a href="https://slavkoplus.plavipixel.com.hr/wp-admin/admin.php?page=tablepress&amp;action=edit&amp;table_id=5">Uredi</a></caption>
+<thead>
+<tr class="row-1 odd">
+	<th class="column-1">Naziv</th><th class="column-2">Dani u tjednu</th><th class="column-3">Satnica</th><th class="column-4">Status / Termin</th><th class="column-5">&nbsp;</th>
+</tr>
+</thead>
+<tbody class="row-hover">
+<tr class="row-2 even">
+	<td colspan="4" class="column-1"><h4>TEČAJEVI</h4></td><td class="column-5">2 rate | jednokratno</td>
+</tr>
+<tr class="row-3 odd">
+	<td colspan="5" class="column-1"><h4>Za upis Afterwork tečaja (termin 17 sati) poklanjamo popust od 25%</h4></td>
+</tr>
+<tr class="row-4 even">
+	<td class="column-1">Afterwork dvomjesečni tečaj - 16 sati </td><td class="column-2">UTO/ČET<br>
+</td><td class="column-3">17:00 - 18:00</td><td class="column-4"><strong>Planirani početak 22.5.</strong></td><td class="column-5">2 mj. 450 kn</td>
+</tr>
+<tr class="row-5 odd">
+	<td colspan="5" class="column-1"></td>
+</tr>
+<tr class="row-6 even">
+	<td class="column-1">COCKTAIL LJETNIH PLESOVA - 8 sati/16 sati</td><td class="column-2">UTO/ČET</td><td class="column-3">19:00 - 21:00</td><td class="column-4"><strong>Planirani početak 24.5.</strong></td><td class="column-5">325 kn | 600 kn</td>
+</tr>
+</tbody>
+</table>
+        </div>
+        <div class="ponuda__content-l">
+          <h3>Plesni tečaj za mladence</h3>
+<p>Bilo da ste mladenci, kumovi ili gost na svadbenoj svečanosti pripremite se za tu priliku kako biste uživali i družili se uz ples cijelu večer. Stoga, prepustite se našim profesionalcima i na vrijeme nas posjetite!</p>
+<p>Svi budući mladenci mogu se odlučiti za jednu od ponuda:</p>
+<p>Naučite plesati: engleski valcer, bečki valcer, foxtrot (dvokorak u kombinaciji s disco-foxom), blues (sentiš).</p>
+<ol>
+<li>BRZI TEČAJ od 3 dana, svaki dan 2 puna sata.</li>
+<li>UBRZANI TEČAJ, 3 tjedna,&nbsp;2&nbsp;dana tjedno po 1,5 sat.</li>
+</ol>
+
+        </div>
+
+        <div class="ponuda__content-r">
+          <h3>Individualna plesna poduka za mladence</h3>
+<p>Idealno je rješenje ukoliko ste ograničeni vremenom ili pak želite posebnu koreografiju za Vaš prvi ples na Vašu omiljenu glazbu.<br>
+Individualnu plesnu poduku radimo s maksimalno 3 plesna para gdje se budućim mladencima mogu priključiti kumovi ili roditelji. Poduka se nudi isključivo u paketima od 3, 5, 7, 10 i 20 sati (<a href="#cjenik-individualna">cjenik za pakete pogledajte niže na stranici</a>)</p>
+<p>Cijena sata za 1 par = 250,00 kn</p>
+<p>Cijena sata za 2 para = 350,00 kn</p>
+<p>Cijena sata za 3 para = 450,00 kn</p>
+<p>*<strong>Popust od 10% za jednokratno plaćanje u gotovini.</strong><br>
+**Ne odnosi se na paket od 3 sata privatne poduke.</p>
+<p>Za one parove ili pojedince kojima je to važno, jamčimo potpunu diskreciju za vrijeme privatne poduke.</p>
+        </div>
+      </div>
+    </section>
+
+</article>
